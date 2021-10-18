91f9eadb5a6765ac538177c4c6589f87c1c39321
JosedeJesus2001 <90534765+JosedeJesus2001@users.noreply.github.com>
2021-10-18 08:17:58 -0500

    Create Punto.h

---
+++ Puntos/Punto.h
@@ -0,0 +1,19 @@
+/*
+ * Author: José de Jesús García Piña
+ * 18/10/2021
+ * Correo: josegarciapina2001@gmail.com
+ */
+
+#ifndef PUNTO_PUNTO_H
+#define PUNTO_PUNTO_H
+
+class Punto2D{
+public:
+    float x, y;
+    void SetPosicion(double a, double b);
+    float Escalar(float temp1, float temp2);
+    [[nodiscard]] float GetX() const, GetY() const;
+    float RotarRespectoAlOrigen(float temp1);
+    float Trasladar (float temp1, float temp2);
+};
+#endif //PUNTO_PUNTO_H
